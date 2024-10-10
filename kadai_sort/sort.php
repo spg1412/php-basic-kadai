d4b236eec1755b903813c4a109a36dbd509fd965
spg1412 <terransullivan@hotmail.com>
2024-10-10 12:33:26 +0900

    Add files via upload

---
+++ kadai_sort/sort.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <title>PHP基礎編</title>
+</head>
+
+<body>
+<p>
+    <?php
+    function sort_2way($array, $order)
+    {
+        if ($order) {
+            echo "昇順にソートします。<br>";
+            sort($array);
+        } else {
+            echo "降順にソートします。<br>";
+            rsort($array);
+        }
+
+        // Show sorted array
+        foreach ($array as $num) {
+            echo $num . " ";
+        }
+    }
+
+    // Usage example
+    $nums = [15, 4, 18, 23, 10];
+    sort_2way($nums, true);  // Ascending sort
+    echo "<br>";
+    sort_2way($nums, false); // Descending sort
+    ?>
+</p>
+</body>
+
+</html>
